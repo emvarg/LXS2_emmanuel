dead63640487bd30726931e018a790063fcc6ae6
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-10-08 10:57:13 -0600

    agrego -f a la hora de eliminar /tmp/archivo1.txt

--- src/parametros_posicionales.sh
+++ src/parametros_posicionales.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ERRORLOG="$0.err"
-ERRORLOG="$PWD/output.err"
+
 # La salida estandar de error estara redireccionada al archivo if-then-sh.err
 
-if cp archivo1.txt archivo1.txt.bck 2> $ERRORLOG
+if cp /tmp/archivo1.txt archivo1.txt.bck 2> $ERRORLOG
 then
 	echo "El archivo1.txt fue correctamente respaldado"
 	echo "Eliminando archivo orginal"
 
-	if rm archivo1.txt
+	if rm -f /tmp/archivo1.txt  2>> $ERRORLOG
 	then
 		echo "El archivo original fue eliminado correctamente.."
 	else
