9027ce077ffb1ea79a7ee91dec66279976166aa5
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-10-08 12:00:26 -0600

    Imprimo los parametros del scritp

--- src/SpecVar.sh
+++ src/SpecVar.sh
@@ -20,4 +20,19 @@ fi
 
 # Quiero ver cuales fueron los argumentos
 
+echo "Los parametros del script fueron:"
+echo " "
+
+VAR1=1
+## Diferencia entre $@ (es un arreglo) y $* (que es un string)
+
+for PARAM in $@
+do
+	echo "El parametro $VAR1 es: $PARAM"
+	let "VAR1=$VAR1+1"
+done
+
+
+echo "VAR1 es :$VAR1"
+
 
