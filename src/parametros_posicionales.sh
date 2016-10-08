c63ff34896deb22ea34bb75f953c716af827a352
Emmanuel Vargas B <emmanuel.vargas@ucr.ac.cr>
2016-10-08 10:44:53 -0600

    caso de if anidados

--- src/parametros_posicionales.sh
+++ src/parametros_posicionales.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-ERRORLOG="output.err"
+ERRORLOG="$0.err"
+ERRORLOG="$PWD/output.err"
 # La salida estandar de error estara redireccionada al archivo if-then-sh.err
 
 if cp archivo1.txt archivo1.txt.bck 2> $ERRORLOG
 then
 	echo "El archivo1.txt fue correctamente respaldado"
+	echo "Eliminando archivo orginal"
+
+	if rm archivo1.txt
+	then
+		echo "El archivo original fue eliminado correctamente.."
+	else
+		echo "No se pudo eliminar el archivo, revisar $ERRORLOG"
+	fi
 else
 	echo "El archivo1.txt no pudo ser respaldado, verifique el log :$ERRORLOG"
 fi
